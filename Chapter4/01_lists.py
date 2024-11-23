eabf9e01ce2575615b3536057aa476f4bc61e618
Kartikey bhatnagar <kartikeybhatnagar247@gmail.com>
2024-11-23 20:16:43 +0530

    lists in python

---
+++ Chapter4/01_lists.py
@@ -0,0 +1,13 @@
+friends = ["Apple","Orange",5,345.56,False,"Aakash","Rohan"]
+print(friends[2])
+print(type(friends[2]))
+
+# Unlike strings, lists are mutable. This means we can change an item in a list by accessing it directly as part of the assignment statement. Using the indexing operator (square brackets) on the left side of an assignment, we can update one of the list items.
+
+#lists elements are mutable by indexing
+friends[0]="Grapes"
+print(friends)
+print(friends[1:4])
+
+# lists are non primitive datatypes in python
+
---
+++ Chapter4/02lists_methods.py
@@ -0,0 +1,20 @@
+friends = ["Apple","Orange",5,345.56,False,"Aakash","Rohan"]
+# print(friends)
+
+friends.append("Harry")
+# print(friends)
+
+l1 =[1,34,562,786,9687]
+# l1.sort()
+# l1.reverse()
+# l1.insert(3,122) # insert krdo 122 at the index 3 list mein
+value = l1.pop(3)
+a = print(value) # print krega wo value jo  index no 3 se pop huyi hai
+print(l1)
+
+# The remove() method removes the first occurrence of the element with the specified value.
+
+# Remove the "banana" element of the fruit list:
+
+fruits = ['apple', 'banana', 'cherry']
+fruits.remove("banana")
